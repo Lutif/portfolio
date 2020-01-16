168cd8e8c499ad4e92b5bf9401f49087ac4803e2
Lutif <lmandhro@gmail.com>
2020-01-16 13:50:16 +0500

    Updates

---
+++ precache-manifest.90436487d3e8dae039bcf267880dbdc1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90cc520bc3a172e4a5d4df6e417a67f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a36d326049e16b7eb95",
+    "url": "/portfolio/static/css/main.63ac9796.chunk.css"
+  },
+  {
+    "revision": "5c1cb2676c54d608e726",
+    "url": "/portfolio/static/js/2.4c3d5053.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.4c3d5053.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a36d326049e16b7eb95",
+    "url": "/portfolio/static/js/main.bda69601.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "a3cbae92d77167a2564882b042a9cccb",
+    "url": "/portfolio/static/media/app-landing.a3cbae92.png"
+  },
+  {
+    "revision": "5259205a40188ba9ddf88af434540132",
+    "url": "/portfolio/static/media/keeper.5259205a.PNG"
+  },
+  {
+    "revision": "61a4c6858f159127641cd8cee0815527",
+    "url": "/portfolio/static/media/main.61a4c685.svg"
+  },
+  {
+    "revision": "a3c0a16223299179625a2b678c64b180",
+    "url": "/portfolio/static/media/shorty.a3c0a162.png"
+  }
+]);
