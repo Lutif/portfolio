a29f7717575b7443065aca1ddf539c023b6ce596
Lutif <lmandhro@gmail.com>
2020-01-24 11:59:02 +0500

    code refactoring

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Main from './components/Main.js';
 import Projects from './components/projects/Projects.js'
 import Skills from './components/Skills.js'
 import Contact from './components/Contact.js'
+import Toggle from './components/Toggle.js'
 function App() {
   return (<div>
     <Nav />
     <Main />
+    <Toggle/>
     <Projects />
     <Skills />
     <Contact/>
